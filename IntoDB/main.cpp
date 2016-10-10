1883bc566394e106d52fb17eb9f44fcf9e562933
xudepeng <xudepengwhu@gmail.com>
2016-10-10 23:29:48 +0800

    use mysql connector c++ api rather than qtsql module

--- IntoDB/main.cpp
+++ IntoDB/main.cpp
@@ -1,72 +1,87 @@
-#include <QtCore/QCoreApplication>
+﻿#include <cstdlib>
+#include <iostream>
 
-#include <QDebug>
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlError>
+/*
+Include directly the different
+headers from cppconn/ and mysql_driver.h + mysql_util.h
+(and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 #include "MotionParser.h"
 
-bool connect(const QString& dbName) {
-	QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-	db.setHostName("localhost");
-	db.setDatabaseName(dbName);
-	db.setUserName("root");
-	db.setPassword("root");
-	if (!db.open()) {
-		qDebug() << "Database Error"
-			<< db.lastError().text();
-		return false;
-	}
-	return true;
-}
+using namespace std;
 
-int main(int argc, char* argv[]) {
-	QCoreApplication a(argc, argv);
-
+int main(int argc, char** argv){
 	MotionParser mp;
 	mp.parse("../data/motion_xy.csv");
+	vector<Pos> pos_lst = mp.getPosition();
 
-	if (!connect("motiontrail")) {
-		return 1;
-	}
+	try {
+		sql::Driver *driver;
+		sql::Connection *con;
+		sql::Statement *stmt;
+		sql::ResultSet *res;
+		sql::PreparedStatement *pstmt;
 
-	QSqlQuery query;
-	if (!query.exec("CREATE TABLE path ("
+		/* Create a connection */
+		driver = get_driver_instance();
+		con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+		/* Connect to the MySQL test database */
+		con->setSchema("test");
+
+		stmt = con->createStatement();
+		stmt->execute("DROP TABLE IF EXISTS path");
+		stmt->execute("CREATE TABLE path ("
 		"id INTEGER PRIMARY KEY AUTO_INCREMENT,"
 		"height INT,"
 		"x DECIMAL(18,6),"
 		"y DECIMAL(18,6),"
-		"state INT)")) {
-		qDebug() << query.lastError().text();
-		return 1;
+		"state INT)");
+		delete stmt;
+
+		con->setAutoCommit(false);
+		/* '?' is the supported placeholder syntax */
+		pstmt = con->prepareStatement("INSERT INTO path (height, x, y, state) VALUES (?, ?, ?, ?)");
+		for (int i = 1; i <= pos_lst.size(); i++) {
+			pstmt->setInt(1,pos_lst[i-1].h);
+			pstmt->setDouble(2,pos_lst[i-1].x);
+			pstmt->setDouble(3,pos_lst[i-1].y);
+			pstmt->setInt(4, (int)pos_lst[i-1].state);
+			pstmt->executeUpdate();
+		}
+		delete pstmt;
+		con->commit();
+
+		/* Select in ascending order */
+		pstmt = con->prepareStatement("SELECT id FROM path ORDER BY id ASC");
+		res = pstmt->executeQuery();
+
+		/* Fetch in reverse = descending order! */
+		res->afterLast();
+		while (res->previous())
+			cout << "\t... MySQL counts: " << res->getInt("id") << endl;
+		delete res;
+
+		delete pstmt;
+		delete con;
+
+	}
+	catch (sql::SQLException &e) {
+		cout << "# ERR: SQLException in " << __FILE__;
+		cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+		cout << "# ERR: " << e.what();
+		cout << " (MySQL error code: " << e.getErrorCode();
+		cout << ", SQLState: " << e.getSQLState() << " )" << endl;
 	}
 
-	query.finish();
-	query.prepare("INSERT INTO path (height, x, y, state) VALUES (?, ?, ?, ?)");
+	cout << endl;
 
-	size_t n_pos = mp.getNPos();
-
-	QVariantList h;
-	QVariantList x;
-	QVariantList y;
-	QVariantList s;
-
-	for (size_t i = 0; i < n_pos; ++i) {
-		Pos cur = mp.getPosition(i);
-		h.push_back(cur.h);
-		x.push_back(cur.x);
-		y.push_back(cur.y);
-		s.push_back((int)cur.state);
-	}
-	query.addBindValue(h);
-	query.addBindValue(x);
-	query.addBindValue(y);
-	query.addBindValue(s);
-
-	if (!query.execBatch()) {
-		qDebug() << query.lastError().text();
-	}
-	query.finish();
-	return a.exec();
+	return EXIT_SUCCESS;
 }
